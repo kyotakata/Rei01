80b9a5c17984a03907beac674527b31cad3bf5a9
kyotakata <57854222+kyotakata@users.noreply.github.com>
2023-12-19 22:12:53 +0900

    7.B03_容器と中身クラスの実装

---
+++ Rei01/Buhin/Youki.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rei01.Buhin
+{
+    internal sealed class Youki : IBuhin
+    {
+        public void Execute()
+        {
+            MessageBox.Show("容器です");
+        }
+    }
+}
