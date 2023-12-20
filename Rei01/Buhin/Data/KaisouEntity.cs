46a883f6d312b78fa35b1f64ea27cfdf0f3a782a
kyotakata <57854222+kyotakata@users.noreply.github.com>
2023-12-20 21:07:55 +0900

    13.B09_階層Entityの作成

---
+++ Rei01/Buhin/Data/KaisouEntity.cs
@@ -0,0 +1,21 @@
+﻿namespace Rei01.Buhin.Data
+{
+    internal sealed class KaisouEntity
+    {
+        public KaisouEntity(int id, int pearentId, int kind, int lebel, string name)
+        {
+            Id = id;
+            PearentId = pearentId;
+            Kind = kind;
+            Lebel = lebel;
+            Name = name;
+        }
+
+        public int Id { get; }
+        public int PearentId { get; }
+        public int Kind { get; }
+        public int Lebel { get; }
+        public string Name { get; }
+
+    }
+}
